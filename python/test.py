2b670125eb74b7b4034fec1896cea5f1c66f40aa
pm <1292040001@qq.com>
2021-04-24 15:03:30 +0800

    添加了文本

--- python/test.py
+++ python/test.py
@@ -2,7 +2,7 @@
 arr = [3, 2, 1, 5, 8, 7, 6]
 temp = ''
 for i in range(len(arr)):
-    for j in range(len(arr)+1-i):
+    for j in range(len(arr)-1-i):
         if arr[j+1] > arr[j]:
             temp = arr[j+1]
             arr[j+1] = arr[j]
